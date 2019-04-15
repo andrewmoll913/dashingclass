78f8697cb96ec9ce6813ffe5feb36ffd0366166a
Andrew Moll <mollandrew913@gmail.com>
2019-04-15 19:55:04 +1200

    Add project files.

---
+++ dashingclass/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace dashingclass.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ dashingclass/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dashingclass.Startup))]
+namespace dashingclass
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}

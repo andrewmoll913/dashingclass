d15fc5745fa9308633070f8ec5e1b32e12a111cf
Andrew Moll <mollandrew913@gmail.com>
2019-04-15 20:09:17 +1200

    Change the layout of the master page.

--- dashingclass/Site.Master.designer.cs
+++ dashingclass/Site.Master.designer.cs
@@ -13,6 +13,15 @@ namespace dashingclass {
     public partial class SiteMaster {
         
         /// <summary>
+        /// CartItemCountLink control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink CartItemCountLink;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>

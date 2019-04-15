f27fdaf93e986479e23a8fae61f9a09ae8a914a1
Andrew Moll <mollandrew913@gmail.com>
2019-04-15 20:34:20 +1200

    Add the shopping cart page.

---
+++ dashingclass/ShoppingCart.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Title="My Shopping Cart" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ShoppingCart.aspx.cs" Inherits="dashingclass.ShoppingCart" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="jumbotron">
+        <div id="ShoppingCartTitle" runat="server" class="ContentHead">
+            <h1>Shopping Cart</h1>
+        </div>
+        <asp:GridView ID="CartList" runat="server"
+            AutoGenerateColumns="False" ShowFooter="True"
+            GridLines="Vertical" CellPadding="4"
+            ItemType="dashingclass.Models.CartItem"
+            SelectMethod="GetShoppingCartItems"
+            CssClass="table table-striped table-bordered">
+            <Columns>
+                <asp:BoundField DataField="ProductId" HeaderText="ID" SortExpression="ProductId" />
+                <asp:BoundField DataField="Product.ProductBrand" HeaderText="Brand" />
+                <asp:BoundField DataField="Product.ProductSize" HeaderText="Size" />
+                <asp:BoundField DataField="Product.ProductColor" HeaderText="Color" />
+                <asp:BoundField DataField="Product.ProductName" HeaderText="Name" />
+                <asp:BoundField DataField="Product.ProductCost" HeaderText="Unit Cost" />
+                <asp:TemplateField HeaderText="Quantity">
+                    <ItemTemplate>
+                        <asp:TextBox ID="PurchaseQuantity" Width="40" runat="server" Text="<%#: Item.Quantity %>"></asp:TextBox>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Item Total">
+                    <ItemTemplate>
+                        <%#: String.Format("{0:c}", ((Convert.ToDouble(Item.Quantity)) *  Convert.ToDouble(Item.Product.ProductCost)))%>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Remove Item">
+                    <ItemTemplate>
+                        <asp:CheckBox ID="Remove" runat="server"></asp:CheckBox>
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+        <div>
+            <p></p>
+            <strong>
+                <asp:Label ID="TotalInformationLabel" runat="server" Text="Order Total: "></asp:Label>
+                <asp:Label ID="TotalLabel" runat="server" EnableViewState="false"></asp:Label>
+            </strong>
+        </div>
+        <br />
+        <table>
+            <tr>
+                <td>
+                    <asp:Button ID="UpdateButton" runat="server" Text="Update" OnClick="UpdateButton_Click" />
+                </td>
+                <td>
+                    <!--Checkout Placeholder -->
+                </td>
+            </tr>
+        </table>
+    </div>
+</asp:Content>
---
+++ dashingclass/ShoppingCart.aspx.cs
@@ -0,0 +1,87 @@
+﻿using dashingclass.Logic;
+using dashingclass.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace dashingclass
+{
+    public partial class ShoppingCart : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (ShoppingCartActions usersShoppingCart = new ShoppingCartActions())
+            {
+                decimal cartTotal = 0;
+                cartTotal = usersShoppingCart.GetTotal();
+                if (cartTotal > 0)
+                {
+                    // Display Total.
+                    TotalLabel.Text = String.Format("{0:c}", cartTotal);
+                }
+                else
+                {
+                    TotalInformationLabel.Text = "";
+                    TotalLabel.Text = "";
+                    ShoppingCartTitle.InnerText = "The shopping Cart is Empty";
+                    UpdateButton.Visible = false;
+                }
+            }
+        }
+
+        public List<CartItem> GetShoppingCartItems()
+        {
+            ShoppingCartActions actions = new ShoppingCartActions();
+            return actions.GetCartItems();
+        }
+
+        public List<CartItem> UpdateCartItems()
+        {
+            using (ShoppingCartActions shoppingCart = new ShoppingCartActions())
+            {
+                String cartId = shoppingCart.GetCartId();
+
+                ShoppingCartActions.ShoppingCartUpdates[] cartUpdates = new ShoppingCartActions.ShoppingCartUpdates[CartList.Rows.Count];
+                for (int i = 0; i < CartList.Rows.Count; i++)
+                {
+                    IOrderedDictionary rowValues = new OrderedDictionary();
+                    rowValues = GetValues(CartList.Rows[i]);
+                    cartUpdates[i].ProductId = Convert.ToInt32(rowValues["ProductId"]);
+
+                    CheckBox cbRemove = new CheckBox();
+                    cbRemove = (CheckBox)CartList.Rows[i].FindControl("Remove");
+                    cartUpdates[i].RemoveItem = cbRemove.Checked;
+
+                    TextBox quantityTextBox = new TextBox();
+                    quantityTextBox = (TextBox)CartList.Rows[i].FindControl("PurchaseQuantity");
+                    cartUpdates[i].PurchaseQuantity = Convert.ToInt16(quantityTextBox.Text.ToString());
+                }
+                shoppingCart.UpdateShoppingCartDatabase(cartId, cartUpdates);
+                CartList.DataBind();
+                TotalLabel.Text = String.Format("{0:c}", shoppingCart.GetTotal());
+                return shoppingCart.GetCartItems();
+            }
+        }
+        public static IOrderedDictionary GetValues(GridViewRow row)
+        {
+            IOrderedDictionary values = new OrderedDictionary();
+            foreach (DataControlFieldCell cell in row.Cells)
+            {
+                if (cell.Visible)
+                {
+                    cell.ContainingField.ExtractValuesFromCell(values, cell, row.RowState, true);
+                }
+            }
+            return values;
+        }
+
+        protected void UpdateButton_Click(object sender, EventArgs e)
+        {
+            UpdateCartItems();
+        }
+    }
+}

4a8762c17b513df13b41500f7a7203b702c1adec
Andrew Moll <mollandrew913@gmail.com>
2019-04-15 20:06:42 +1200

    Add the database initializer to the Global startup file.

--- dashingclass/Global.asax.cs
+++ dashingclass/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using dashingclass.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Optimization;
@@ -16,6 +18,9 @@ namespace dashingclass
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Initialize the product database.
+            Database.SetInitializer(strategy: new ProductDatabaseInitializer());
         }
     }
 }

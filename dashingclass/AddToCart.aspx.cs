4354823f1f6b0167e3591eb44e57e8053633fd61
Andrew Moll <mollandrew913@gmail.com>
2019-04-15 20:36:58 +1200

    Add the add to cart feature.

---
+++ dashingclass/AddToCart.aspx.cs
@@ -0,0 +1,33 @@
+﻿using dashingclass.Logic;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace dashingclass
+{
+    public partial class AddToCart : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string rawId = Request.QueryString["productID"];
+            int productId;
+            if (!String.IsNullOrEmpty(rawId) && int.TryParse(rawId, out productId))
+            {
+                using (ShoppingCartActions shoppingCart = new ShoppingCartActions())
+                {
+                    shoppingCart.AddToCart(Convert.ToInt16(rawId));
+                }
+            }
+            else
+            {
+                Debug.Fail("ERROR : Users should never go to this page if they never selected a product");
+                throw new Exception("ERROR : It is illegal to load AddToCart.aspx without setting a ProductId.");
+            }
+            Response.Redirect("ShoppingCart.aspx");
+        }
+    }
+}

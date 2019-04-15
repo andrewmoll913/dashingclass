dbdb206c8b0683e6f4e1c35655d4a7fcb4422de7
Andrew Moll <mollandrew913@gmail.com>
2019-04-15 20:01:03 +1200

    Add the ProductContext class.

---
+++ dashingclass/Models/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace dashingclass.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("DashingClass")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
